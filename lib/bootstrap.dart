8bd69b288e9a15d8ff9e81c53376700b5dc22f0a
Ngonidzashe Mangudya <imngonii@gmail.com>
2024-03-30 23:43:42 +0200

    feat: register shared blocs

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -1,8 +1,19 @@
 import 'dart:async';
 import 'dart:developer';
+import 'dart:io';
 
-import 'package:bloc/bloc.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:handy_extensions/handy_extensions.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
+import 'injection.dart';
+import 'providers.dart';
+import 'shared/configs/constants.dart';
+import 'shared/services/notifications.dart';
+import 'shared/services/storage.dart';
+import 'shared/utils/values.dart';
 
 class AppBlocObserver extends BlocObserver {
   const AppBlocObserver();
@@ -21,13 +32,50 @@ class AppBlocObserver extends BlocObserver {
 }
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isAndroid) {
+    await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    log('Setting immersive sticky mode for Android ✅');
+  }
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]);
+  log('Setting preferred orientations ✅');
+
+  configureInjection();
+
   FlutterError.onError = (details) {
     log(details.exceptionAsString(), stackTrace: details.stack);
+    Sentry.captureException(details.exception, stackTrace: details.stack);
   };
 
   Bloc.observer = const AppBlocObserver();
 
-  // Add cross-flavor configuration here
+  await getIt<PushNotifications>().init();
 
-  runApp(await builder());
+  if (getIt<StorageService>().getFromDisk('first_time') == null) {
+    final String title = $welcomeTitles.randomItem();
+    final String body = $welcomeBodies.randomItem();
+    await getIt<PushNotifications>().showNotification(title, body);
+    getIt<StorageService>().saveToDisk('first_time', true);
+  }
+
+  await SentryFlutter.init(
+    (options) {
+      options
+        ..dsn = AppConstants.sentryDsn
+        ..tracesSampleRate = 1.0;
+    },
+    appRunner: () async => runApp(
+      MultiBlocProvider(
+        providers: providers,
+        child: SentryScreenshotWidget(
+          child: await builder(),
+        ),
+      ),
+    ),
+  );
 }
---
+++ lib/providers.dart
@@ -0,0 +1,22 @@
+//
+//  shakesphere
+//  providers
+//
+//  Created by Ngonidzashe Mangudya on 30/03/2024.
+//  Copyright (c) 2024 ModestNerds, Co
+//
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'injection.dart';
+import 'shared/state/connectivity_status/connectivity_status_bloc.dart';
+import 'shared/state/locale/locale_bloc.dart';
+
+List<BlocProvider> providers = [
+  BlocProvider<LocaleBloc>(
+    create: (_) => getIt<LocaleBloc>(),
+  ),
+  BlocProvider<ConnectivityStatusBloc>(
+    create: (_) => getIt<ConnectivityStatusBloc>(),
+  ),
+];
